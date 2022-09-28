7c03293fad20a956e7d4b27252f789eeb9b00246
Kamesh Kotwani <kameshkotwani@outlook.com>
2022-09-28 18:56:45 +0530

    removed redundant directory creation

--- airflow-init-once.sh
+++ airflow-init-once.sh
@@ -3,10 +3,6 @@ set -ex
 
 echo "RUN THIS FILE ONLY ONCE"
 
-echo "Creating directories"
-
-mkdir -p ./dags ./plugins ./logs
-
 echo "Creating postgres db and airflow user"
 docker compose up -d airflow-init
 
