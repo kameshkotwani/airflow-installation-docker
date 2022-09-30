640d36f4b15b70ae6516f9383071bf5a2ed2678d
Kamesh Kotwani <kameshkotwani@outlook.com>
2022-09-30 22:00:28 +0530

    deleted unwanted scripts

--- airflow-init-once.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -ex
-
-echo "RUN THIS FILE ONLY ONCE"
-
-echo "Creating postgres db and airflow user"
-docker compose up --profile flower -d airflow-init
-
-echo "database created, please run start-airflow.sh to start your airflow instance"
--- create-airflow-containers-once.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -ex
-
-echo "Starting Containers"
-
-docker compose --profile flower up -d
-
-echo "containers started"
