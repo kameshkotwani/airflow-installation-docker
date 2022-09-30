f6530b571746893fefbd194f8f70a73724ca043b
Kamesh Kotwani <kameshkotwani@outlook.com>
2022-09-30 21:55:55 +0530

    renamed file according to steps

--- 1.airflow-init-once.sh
+++ 1.airflow-init-once.sh
@@ -6,4 +6,4 @@ echo "RUN THIS FILE ONLY ONCE"
 echo "Creating postgres db and airflow user"
 docker compose up --profile flower -d airflow-init
 
-echo "database created, please run start-airflow.sh to start your airflow instance"
+echo "database created, please run 2.create-airflow-containers-once.sh to start your airflow instance"
