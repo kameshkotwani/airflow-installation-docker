2a5ec94b376a475420482cd53057625ce8f1573f
Kamesh Kotwani <36355951+kameshkotwani@users.noreply.github.com>
2023-05-02 22:11:51 +0530

    fixed init command error

--- 1.airflow-init-once.sh
+++ 1.airflow-init-once.sh
@@ -4,6 +4,6 @@ set -ex
 echo "RUN THIS FILE ONLY ONCE"
 
 echo "Creating postgres db and airflow user"
-docker compose up --profile flower -d airflow-init
+docker compose --profile flower up -d airflow-init
 
 echo "database created, please run 2.create-airflow-containers-once.sh to start your airflow instance"
